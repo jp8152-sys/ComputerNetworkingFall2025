d2c26edacd63aad91a7cca55f30d84eb9f688356
jp8152-sys <jp8152@nyu.edu>
2025-09-08 21:07:32 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,36 +1,36 @@
 def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-        print(welcome_assignment_answers(asnwer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
         answer = "8152"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "Is MD5 a secured hashing algorithm?":
         answer = "No"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = "7"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to?":
         answer = "3"
-        print(welcome_assignment_answers(aswer))
+        print(welcome_assignment_answers(answer))
