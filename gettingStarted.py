f09c55575f431b047e7d49b912ff061a59980108
jp8152-sys <jp8152@nyu.edu>
2025-09-12 20:59:52 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,19 +1,23 @@
 def welcome_assignment_answers(question):
-    answers = {
-        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "pcap",
-        "Are encoding and encryption the same? - Yes/No": "No",
-        "Is it possible to decrypt a message without a key? - Yes/No": "No",
-        "Is it possible to decode a message without a key? - Yes/No": "Yes",
-        "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
-        "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": "b92d7fde9b860d26b779dd51c91e0803be956a5b29f5bc33341c909fec15b9fc",
-        "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": "4",
-        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": "3",
-    }
-
-    return answers.get(question, "Question not recognized.")
-
-
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        return "pcap"
+    if question == "Are encoding and encryption the same? - Yes/No":
+        return "No"
+    if question == "Is it possible to decrypt a message without a key? - Yes/No":
+        return "No"
+    if question == "Is it possible to decode a message without a key? - Yes/No":
+        return "Yes"
+    if question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        return "No"
+    if question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        return "b92d7fde9b860d26b779dd51c91e0803be956a5b29f5bc33341c909fec15b9fc"
+    if question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        return "No"
+    if question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        return 4
+    if question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        return 3
+    return ""
 
 
 
