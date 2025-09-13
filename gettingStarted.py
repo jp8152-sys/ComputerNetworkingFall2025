74ccd4677de148b6b742795cb7b1607140255299
jp8152-sys <jp8152@nyu.edu>
2025-09-12 20:47:35 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -7,11 +7,12 @@ def welcome_assignment_answers(question):
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
         "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": "b92d7fde9b860d26b779dd51c91e0803be956a5b29f5bc33341c909fec15b9fc",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4,
-        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3,
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": "4",
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": "3",
     }
 
     return answers.get(question, "Question not recognized.")
+
 
 
 
